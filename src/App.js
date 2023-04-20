cb472ee7f065cd4dab9f55d155c7c6486828beb7
justinianoleo <justinianoleo@gmail.com>
2023-04-20 08:59:45 -0400

    ADDING A COMMIT

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import Hero from "./components/Hero";
 import { Footer } from "./components/Footer";
+import Home from "./Home";
 
 import { Header } from "./components/Header";
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/home" element={<Home />} />
+      </Routes>
       <Header />
       <main>
         <Hero />
